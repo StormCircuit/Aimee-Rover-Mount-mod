0f2cb7fc9bba32999432843cdbf19b74dc9c93c0
Storm <tjaccardi98@gmail.com>
2026-07-16 08:51:49 -0400

    Cleaned up the entrypoint file

--- aimeeUber/Patches/aimeeUberMod.cs
+++ aimeeUber/Patches/aimeeUberMod.cs
@@ -1,51 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using Assets.Scripts.Objects;
 using UnityEngine;
 
-namespace aimeeUberMod
-{
+namespace aimeeUberMod {
     [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-    public class aimeeUberModPlugin : BaseUnityPlugin
-    {
-      public const string pluginGuid = "com.username.aimeeUberMod";
-      public const string pluginName = "aimeeUberMod";
-      public const string pluginVersion = "1.0";
+    public class aimeeUberModPlugin : BaseUnityPlugin {
+        public const string pluginGuid = "com.username.aimeeUberMod";
+        public const string pluginName = "aimeeUberMod";
+        public const string pluginVersion = "1.0";
 
-      public static ManualLogSource Log;
-      public static Boolean modEnabled;
+        public static ManualLogSource Log;
+
+        //used to offset Aimee specifically, solves Issue 3
         public const float RoverMountOffsetY = -0.5f;
-      public const float RoverMountPitchTrimDegrees = 25f;
 
-      void Awake()
-      {
-        try
-        {
-          Log = Logger;
+        //used to pitch aimee a little when mounted part of Issue 3
+        public const float RoverMountPitchTrimDegrees = 25f;
 
-          var harmony = new Harmony(pluginGuid);
-          harmony.PatchAll();
-          Log.LogInfo("Patch succeeded");
+        void Awake() {
+            try {
+                Log = Logger;
+
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log.LogInfo("Patch succeeded");
+            }
+            catch (Exception e) {
+                if (Log != null) {
+                    Log.LogError("Patch Failed");
+                    Log.LogError(e.ToString());
+                }
+                else {
+                    Debug.LogError("[" + pluginName + "]: Patch Failed");
+                    Debug.LogError(e.ToString());
+                }
+            }
         }
-        catch (Exception e)
-        {
-          if (Log != null)
-          {
-            Log.LogError("Patch Failed");
-            Log.LogError(e.ToString());
-          }
-          else
-          {
-            Debug.LogError("[" + pluginName + "]: Patch Failed");
-            Debug.LogError(e.ToString());
-          }
-        }
-      }
     }
 }
